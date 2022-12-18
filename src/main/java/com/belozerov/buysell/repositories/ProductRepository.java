855025d989ec7b883c285897dc42d5d3ac8c327f
Mikhail <belozerov.mg@gmail.com>
2022-12-19 00:37:19 +0700

    Connecting the application to the database

---
+++ src/main/java/com/belozerov/buysell/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.belozerov.buysell.repositories;
+
+import com.belozerov.buysell.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByTitle(String title);
+}
--- src/main/java/com/belozerov/buysell/services/ProductService.java
+++ src/main/java/com/belozerov/buysell/services/ProductService.java
@@ -1,38 +1,36 @@
 package com.belozerov.buysell.services;
 
 import com.belozerov.buysell.models.Product;
+import com.belozerov.buysell.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class ProductService {
-    private List<Product> products = new ArrayList<>();
-    private long ID = 0;
 
-    {
-        products.add(new Product(++ID, "Iphone 11", "Simple description", 40000, "Kazan", "Eugeni"));
-        products.add(new Product(++ID, "TV", "Simple description", 25000, "Omsk", "Alexei"));
-    }
+    private final ProductRepository productRepository;
 
-    public List<Product> listProducts() {
-        return products;
+    public List<Product> listProducts(String title) {
+        if (title != null) return productRepository.findByTitle(title);
+        return productRepository.findAll();
     }
 
     public Product getProductById(Long id) {
-        for (Product product : products) {
-            if (product.getId().equals(id)) return product;
-        }
-        return null;
+        return productRepository.findById(id).orElse(null);
     }
 
     public void saveProduct(Product product) {
-        product.setId(++ID);
-        products.add(product);
+        log.info("Saving new {}", product);
+        productRepository.save(product);
     }
 
     public void deleteProduct(Long id) {
-        products.removeIf(product -> product.getId().equals(id));
+        productRepository.deleteById(id);
     }
 }
